5838bc11ab56c37093a3f461215e3441d9d7b631
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-11 12:11:58 -0700

    project get works but still need to get true false working

--- database/helpers/projectModel.js
+++ database/helpers/projectModel.js
@@ -8,18 +8,18 @@ module.exports = {
 
 function find() {
     return db('projects')
-        .then(projects => {
-            const converter = []// will hold new array of objs
-            projects.forEach(project => {
-                if (project.completed === 1) {
-                    return project.completed = true
-                } else {
-                    return project.completed = false
-                }
-            })
-            converter.push(project)//push the new array of objects
-            return converter
-        })
+    // .then(projects => {
+    //     const converter = []// will hold new array of objs
+    //     projects.forEach(project => {
+    //         if (project.completed === 1) {
+    //             return project.completed = true
+    //         } else {
+    //             return project.completed = false
+    //         }
+    //     })
+    //     converter.push(project)//push the new array of objects
+    //     return converter
+    // })
 }
 
 function findById(id) {
