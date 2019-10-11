e86214fb31996747d9a1c3a831a4a5322344bf19
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-11 08:21:39 -0700

    server is running

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,53 @@
-// Update with your config settings.
+const localPg = {
+  host: 'localhost',
+  port: 5000,
+  user: 'dan',
+  database: 'bussiness_api',
+};
+
+const pgUser = process.env.PG_USER || 'dan';
+const pgDb = process.env.PG_DB || 'bussiness_api';
+
+const prodConnection = `postgres://${pgUser}@localhost/${pgDb}`;
+
+
+
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './database/file/bussiness_api.db3',
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // enforces foreign key constraints on SQLite, not needed for other DBMS
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: probConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   }
 
 };
