793aef19f883517d88aac6fe4eb6d231dc206ecc
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-11 11:27:08 -0700

    building porject get

--- api/routes/projectsRoute.js
+++ api/routes/projectsRoute.js
@@ -1,1 +1,18 @@
-const express = require('express')
+const express = require('express');
+
+const Projects = require('../../database/helpers/projectModel');
+
+const router = express.Router()
+
+const errorRef = require('../../middleware/errorRef');
+//Get request
+router.get('/', (req, res) => {
+    Projects.find()
+        .then(projects => {
+            res.status(200).json(projects)
+        }).catch(error => {
+            res.status(500).json(errorRef(err))
+        })
+})
+
+module.exports = router
---
+++ database/helpers/projectModel.js
@@ -0,0 +1,9 @@
+const db = require('../../database/dbConfig');
+
+module.exports = {
+    find,
+    findById,
+    add
+}
+
+funtion find
